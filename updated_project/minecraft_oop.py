c19815ff6b2251e50a7e209c32493b2e5f88afaa
USER <eugenemabansag8@gmail.com>
2026-06-21 16:24:54 +0800

    Changed boxes and box variables to private

--- updated_project/minecraft_oop.py
+++ updated_project/minecraft_oop.py
@@ -8,25 +8,25 @@ class MainGame(urs.Entity):
   # create construct to initialize variables
   def __init__(self):
     super().__init__(self)
-    self.boxes = []
+    self.__boxes = []
     for i in range(20):
       for j in range(20):
         # minor adjustments for the change in importing the module
-        self.box = urs.Button(color=urs.color.white, model='cube', position=(j,0,i),
+        self.__box = urs.Button(color=urs.color.white, model='cube', position=(j,0,i),
               texture='grass.png', parent=urs.scene, origin_y=0.5)
-        self.boxes.append(self.box)
+        self.__boxes.append(self.__box)
 
   # method for player control
   def input(self,key):
-    for box in self.boxes:
+    for box in self.__boxes:
       if box.hovered:
         if key == 'left mouse down':
           # minor adjustments for the change in importing the module
           new = urs.Button(color=urs.color.white, model='cube', position=box.position + urs.mouse.normal,
                       texture='grass.png', parent=urs.scene, origin_y=0.5)
-          self.boxes.append(new)
+          self.__boxes.append(new)
         if key == 'right mouse down':
-          self.boxes.remove(box)
+          self.__boxes.remove(box)
           urs.destroy(box)
 
 if __name__ == "__main__":
