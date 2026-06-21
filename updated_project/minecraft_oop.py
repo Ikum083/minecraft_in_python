27f6618120b86514f2e5415839c3e5bb54b2faec
USER <eugenemabansag8@gmail.com>
2026-06-21 15:59:55 +0800

    Added main class and construct

--- updated_project/minecraft_oop.py
+++ updated_project/minecraft_oop.py
@@ -1,15 +1,20 @@
-from ursina import *
+# instead of importing everything from the ursina engine we use import ursina as urs to avoid problems
+import ursina as urs
 from ursina.prefabs.first_person_controller import FirstPersonController
+
 app = Ursina()
 player = FirstPersonController()
 Sky()
-
-boxes = []
-for i in range(20):
-  for j in range(20):
-    box = Button(color=color.white, model='cube', position=(j,0,i),
-          texture='grass.png', parent=scene, origin_y=0.5)
-    boxes.append(box)
+# create main class
+class MainGame:
+  # create construct to initialize variables
+  def __init__(self):
+    self.boxes = []
+    for i in range(20):
+      for j in range(20):
+        self.box = urs.Button(color=color.white, model='cube', position=(j,0,i),
+              texture='grass.png', parent=scene, origin_y=0.5)
+        self.boxes.append(box)
 
 def input(key):
   for box in boxes:
